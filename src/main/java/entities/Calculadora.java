bb65e331fc87dea0386337c83c02240a7de11214
estudiante <marconoguero@gmail.com>
2021-04-19 10:40:22 +0200

    suma,resta,multi,divi TEST

---
+++ src/main/java/entities/Calculadora.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Calculadora {
+
+    public int suma(int a, int b){
+        return a+b;
+    }
+
+    public int resta(int b, int c){
+
+        return b-c;
+    }
+
+    public int multiplicacion(int a, int b){
+        return a*b;
+    }
+    public int division(int a, int b){
+        return a/b;
+    }
+
+
+
+
+
+}
---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,34 @@
+import entities.Calculadora;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void pruebaSuma(){
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.suma(3,2);
+        assertEquals(5,resultado);
+    }
+
+    @Test
+    public void pruebaResta(){
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.resta(5,2);
+        assertEquals(3,resultado);
+    }
+    @Test
+    public void pruebaMulti(){
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.multiplicacion(5,5);
+        assertEquals(25,resultado);
+    }
+    @Test
+    public void pruebaDivision(){
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.division(25,5);
+        assertEquals(5,resultado);
+    }
+
+}
